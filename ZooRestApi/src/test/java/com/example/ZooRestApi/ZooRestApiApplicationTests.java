2cba7f14015a16dbb9ebe35374da8669e0576b28
skaimarvas <skaimarvas@gmail.com>
2024-02-07 10:03:25 +0300

    server.port was changed to 9000

---
+++ ZooRestApi/src/test/java/com/example/ZooRestApi/ZooRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZooRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
