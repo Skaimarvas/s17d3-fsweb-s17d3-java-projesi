2e912ab343d3caf2cdf17b2d0fbd5cb562a36799
skaimarvas <skaimarvas@gmail.com>
2024-02-07 11:43:51 +0300

    Koala(concrete) class was created.

---
+++ zoo/src/main/java/com/example/zoo/entity/Koala.java
@@ -0,0 +1,12 @@
+package com.example.zoo.entity;
+
+
+public class Koala {
+    private long id;
+    private String name;
+    private int weight;
+    private String gender;
+    private boolean isAggressive;
+
+
+}
