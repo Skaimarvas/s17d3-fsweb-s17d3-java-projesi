e6936c02049225469f31d8902541493678398a97
skaimarvas <skaimarvas@gmail.com>
2024-02-07 13:13:16 +0300

    Kangaroo (subclass) was created.

---
+++ zoo/src/main/java/com/example/zoo/entity/Kangaroo.java
@@ -0,0 +1,12 @@
+package com.example.zoo.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Kangaroo extends Animal {
+
+    private boolean isAggressive;
+}
