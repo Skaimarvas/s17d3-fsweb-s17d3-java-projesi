f66e9d6bf6f1e78bb15ded65b5a547e38ee03607
skaimarvas <skaimarvas@gmail.com>
2024-02-07 15:09:29 +0300

    Constructor was added manually.

--- zoo/src/main/java/com/example/zoo/entity/Koala.java
+++ zoo/src/main/java/com/example/zoo/entity/Koala.java
@@ -3,11 +3,19 @@ package com.example.zoo.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
+
 
 @AllArgsConstructor
 @Data
+
 public class Koala extends Animal {
 
     private int sleepHour;
 
+
+    public Koala(long id, String name, int weight, int height, Gender gender, int sleepHour) {
+        super(id, name, weight, height, gender);
+        this.sleepHour = sleepHour;
+    }
 }
