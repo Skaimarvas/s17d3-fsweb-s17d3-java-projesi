7e37431d77a4a5626ce24c007af2554b3667b066
skaimarvas <skaimarvas@gmail.com>
2024-02-07 12:02:37 +0300

    New notes, fields and Lombok annotations were added.

--- zoo/src/main/java/com/example/zoo/exceptions/ZooErrorResponse.java
+++ zoo/src/main/java/com/example/zoo/exceptions/ZooErrorResponse.java
@@ -1,4 +1,25 @@
 package com.example.zoo.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**Notes:
+ * @NoArgsConstructor instructs Lombok to generate empty constructor
+ * @AllArgsConstructor instructs Lombok to generate constructor contains all fields in related class
+ * @Getter instructs Lombok to generate getter method
+ * @Setter instructs Lombok to generate setter method
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ZooErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime createdAt;
 }
