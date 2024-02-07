47da7d5ff1d4704da1fb0229c6296261e185182c
skaimarvas <skaimarvas@gmail.com>
2024-02-07 12:02:04 +0300

    new note was added

--- zoo/src/main/java/com/example/zoo/exceptions/ZooException.java
+++ zoo/src/main/java/com/example/zoo/exceptions/ZooException.java
@@ -4,12 +4,12 @@ package com.example.zoo.exceptions;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-/**
+/**Notes:
  * Why did we add @Getter annotation? Explain:
  * This annotation instructs Lombok
  * to generate getter method instead generate it manually
  * ---------------------------------------------------------
- * Why did we extend Runtime Exception? :
+ * Why did we extend Runtime Exception?:
  * to handle runtime exception error?
  *
  */
